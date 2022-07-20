38ea200d808ffccc3de0abeb7e3a363f07e9fdae
TEX APlayer <109390064+TEXADeveloper@users.noreply.github.com>
2022-07-20 20:43:49 -0300

    Made movement sensible to forces

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,38 @@
+using System.Diagnostics.Tracing;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [HideInInspector] private PlayerController pController;
     [HideInInspector] public float Direction;
     [SerializeField] private float speed;
     private Rigidbody2D rb;
+    private bool impulsed = false;
+
+    public void Impulse(Vector2 force)
+    {
+        rb.AddForce(force, ForceMode2D.Impulse);
+        impulsed = true;
+    }
 
     void Start()
     {
+        pController = this.GetComponent<PlayerController>();
         rb = this.GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         SetVelocity();
     }
 
     private void SetVelocity()
     {
-        rb.velocity = new Vector2(Direction * speed, rb.velocity.y);
+        if (!impulsed)
+            rb.velocity = new Vector2(Direction * speed, rb.velocity.y);
+        else if (impulsed && (Mathf.Abs(rb.velocity.x) <= 1 || Time.time <= pController.pState.GetTimeGrounded() + 0.2f))
+            impulsed = false;
     }
 }
